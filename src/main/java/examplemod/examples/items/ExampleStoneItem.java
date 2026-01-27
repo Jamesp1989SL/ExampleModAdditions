9763922df97805645b3cc6c4b12209f646ed75be
Jamesp1989SL <useruser3.pollock@gmail.com>
2026-01-27 22:09:11 +0000

    clean

--- src/main/java/examplemod/examples/items/ExampleStoneItem.java
+++
@@ -1,9 +0,0 @@
-package examplemod.examples.items;
-
-import necesse.inventory.item.placeableItem.StonePlaceableItem;
-
-public class ExampleStoneItem extends StonePlaceableItem {
-   public ExampleStoneItem(){
-    super(100);
-   }
-}
