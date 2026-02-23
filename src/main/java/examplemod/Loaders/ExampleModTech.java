2ee15b68ec8de14880dc48c6b9d295990cebabd6
Mads Skovgaard <madsskovgaard@hotmail.com>
2026-02-23 11:04:52 +0100

    Adds missing tech locale key.

--- src/main/java/examplemod/Loaders/ExampleModTech.java
+++ src/main/java/examplemod/Loaders/ExampleModTech.java
@@ -1,19 +1,19 @@
 package examplemod.Loaders;
 
-import examplemod.examples.buffs.ExampleArmorSetBuff;
-import examplemod.examples.buffs.ExampleArrowBuff;
-import examplemod.examples.buffs.ExampleBuff;
-import examplemod.examples.buffs.ExampleTrinketBuff;
-import necesse.engine.registries.BuffRegistry;
 import necesse.engine.registries.RecipeTechRegistry;
 import necesse.inventory.recipe.Tech;
 
 public class ExampleModTech {
+
         public static Tech EXAMPLE_TECH;
 
         public static void load(){
                 // stringID: how recipes refer to it internally
                 // itemStringID: used for icon/tooltips (usually your crafting station item id)
                 EXAMPLE_TECH = RecipeTechRegistry.registerTech("exampletech", "examplecraftingstation");
+
+                // Remember to also add the tech to your locale file. The name of the tech will be
+                // shown in the crafting guide book, etc. (This is already done in this example)
         }
+
 }
